c2ba25878852aaa100a60fc88e47f127ecd2eeff
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2018-09-15 16:25:28 -0300

    [ADD} Basic Walk animation controller

---
+++ Hugo/Super Mario World/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    #region Variables
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private bool walking;
+    public bool Walking {
+        get { return walking; }
+        set {
+            walking = value;
+            animator.SetBool( "walking", value );
+        }
+    }
+
+    #endregion Variables
+
+    void Awake () {
+        animator = gameObject.GetComponent<Animator>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float horz = Input.GetAxis("Horizontal");
+
+        if (horz != 0f ) {
+            Walking = true;
+
+            if(horz > 0 ) {
+                spriteRenderer.flipX = false;
+            } else {
+                spriteRenderer.flipX = true;
+            }
+
+        } else {
+            Walking = false;
+        }
+    }
+}
