c93fb0371c4349474eee0061c2febdff25580780
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2018-09-16 16:53:08 -0300

    [ADD] Jump Animation

--- Hugo/Sonic Advanced/Assets/Scripts/PlayerController.cs
+++ Hugo/Sonic Advanced/Assets/Scripts/PlayerController.cs
@@ -6,19 +6,28 @@ public class PlayerController : MonoBehaviour {
 
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigidbody2D;
     private bool walking;
+
+    // Checks the ground collision
+    public Transform groundCheck;
+    private bool grounded;
+    public LayerMask whatIsGround;
 
     [SerializeField]
     private float speed = 2f;
+    [SerializeField]
+    private float jumpImpulse = 100f;
 
     // Use this for initialization
     void Awake() {
         animator = gameObject.GetComponent<Animator>();
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void FixedUpdate () {
         float horizontal = Input.GetAxis("Horizontal");
         
         if(horizontal != 0 ) {
@@ -31,8 +40,12 @@ public class PlayerController : MonoBehaviour {
             animator.SetBool( "walking", false );
         }
 
-        if ( Input.GetButtonDown( "Jump" ) ) {
-            animator.SetBool( "jumping", true );
+        grounded = Physics2D.OverlapCircle( groundCheck.position, 0.001f, whatIsGround );
+
+        animator.SetBool( "jumping", !grounded );
+
+        if ( Input.GetButtonDown( "Jump" ) && grounded ) {
+            rigidbody2D.AddForce( Vector2.up * jumpImpulse );
         }
 
 	}
