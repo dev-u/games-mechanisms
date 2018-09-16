e5f95a29d2e1e07200c876606ff4c23906c2135d
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2018-09-16 12:02:10 -0300

    [ADD] Very simple jump

--- Hugo/Super Mario World/Assets/Scripts/PlayerController.cs
+++ Hugo/Super Mario World/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,24 @@ public class PlayerController : MonoBehaviour {
 
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigidbody2D;
+
+    [SerializeField]
+    private int jumpForce = 150;
+
+    // Verifies the ground
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+    public bool grounded;
+
+    private bool jumping;
+    public bool Jumping {
+        get { return jumping; }
+        set {
+            jumping = value;
+            animator.SetBool( "jumping", value );
+        }
+    }
 
     private bool walking;
     public bool Walking {
@@ -23,10 +41,11 @@ public class PlayerController : MonoBehaviour {
     void Awake () {
         animator = gameObject.GetComponent<Animator>();
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         float horz = Input.GetAxis("Horizontal");
 
         if (horz != 0f ) {
@@ -40,6 +59,13 @@ public class PlayerController : MonoBehaviour {
 
         } else {
             Walking = false;
+        }
+
+        grounded = Physics2D.OverlapCircle( groundCheck.position, 0.2f, whatIsGround );
+        Jumping = !grounded;
+
+        if ( Input.GetButtonDown( "Jump" ) ) {
+            rigidbody2D.AddForce( Vector2.up * jumpForce );
         }
     }
 }
