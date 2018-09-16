54747b6c1387d370fc8589576ac7c388421f1418
Andressa Camargo <dessaccamargo@gmail.com>
2018-09-16 16:53:52 -0300

    [ADD] Jumping

--- Andressa/Sonic Advance/Assets/Scripts/PlayerController.cs
+++ Andressa/Sonic Advance/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,24 @@ public class PlayerController : MonoBehaviour {
     private Animator animator;
     private SpriteRenderer spriteRenderer;
     private bool walking;
-
+    private Rigidbody2D rigidbody2D;
     [SerializeField]
-    private float speed = 2;
+    private float speed = 2f;
+    [SerializeField]
+    private float jumpImpulse = 100f;
+
+    public Transform groundCheck;
+    public LayerMask whatIsGround; 
+    
+    private bool reachedGround;
+
+    
 
     // Use this for initialization
     private void Awake(){
         animator = gameObject.GetComponent<Animator>();
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Start () {
@@ -22,7 +32,7 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         float horizontal = Input.GetAxis("Horizontal");
 
         walking = horizontal != 0 ? true : false;
@@ -36,7 +46,12 @@ public class PlayerController : MonoBehaviour {
             animator.SetBool("walking", false);
         }
 
-        if(Input.GetButtonDown("Jump")){
+        reachedGround = Physics2D.OverlapCircle(groundCheck.position, 0.001f, whatIsGround);
+
+        animator.SetBool("jumping", !reachedGround);
+
+        if(Input.GetButtonDown("Jump") && reachedGround){
+            rigidbody2D.AddForce(Vector2.up * jumpImpulse);
             animator.SetBool("jumping", true);
         }
 	}
