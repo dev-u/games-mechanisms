976ecaeff3f585740cc58146050272416c62754b
Hugo Uchôas Borges <hugoborges@unifei.edu.br>
2018-09-17 00:44:04 -0300

    [ADD] FastWalking and Better Running

--- Hugo/Super Mario World/Assets/Scripts/PlayerController.cs
+++ Hugo/Super Mario World/Assets/Scripts/PlayerController.cs
@@ -6,26 +6,33 @@ public class PlayerController : MonoBehaviour {
 
     #region Variables
 
+    // GameObject Components
     private Animator animator;
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rigidbody2D;
     private BoxCollider2D boxCollider2D;
 
+    // PlayerController Variables
     [SerializeField]
-    private int jumpForce = 150;
+    private int jumpForce = 165;
     private float currentSpeed;
     [SerializeField]
-    private float walkSpeed = 0.4f;
+    private float walkSpeed = 0.65f;
     [SerializeField]
-    private float fastWalkSpeed = 1f;
+    private float fastWalkSpeed = 1.3f;
     [SerializeField]
-    private float runSpeed = 1.5f;
+    private float runSpeed = 2f;
+    [SerializeField]
+    private float timeUntilMaxSpeed = 0.6f;
+    private float currentTime = 0f;
 
     // Verifies the ground
     public Transform groundCheck;
     public LayerMask whatIsGround;
     public bool grounded;
 
+
+    // Animation Controller Variables
     private bool jumping;
     public bool Jumping {
         get { return jumping; }
@@ -35,23 +42,45 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
-    private bool walking;
-    public bool Walking {
-        get { return walking; }
-        set {
-            walking = value;
-            animator.SetBool( "walking", walking );
-            if( walking )
-                currentSpeed = walkSpeed;
-        }
-    }
-
     private bool falling;
     public bool Falling {
         get { return falling; }
         set {
             falling = value;
             animator.SetBool( "falling", falling );
+            if ( falling ) {
+                if ( !Jumping ) {
+                    currentTimePenalty( timeUntilMaxSpeed / 3 );
+                } else if ( !Running ) {
+                    currentTimePenalty( timeUntilMaxSpeed / 2 );
+                }
+
+            }
+        }
+    }
+
+    private bool walking;
+    public bool Walking {
+        get { return walking; }
+        set {
+            walking = value;
+            animator.SetBool( "walking", walking );
+        }
+    }
+
+    private bool fastWalking;
+    public bool FastWalking {
+        get { return fastWalking; }
+        set {
+            fastWalking = value;
+            animator.SetBool( "fastwalking", fastWalking );
+
+            if ( fastWalking ) {
+                currentSpeed = fastWalkSpeed;
+            } else {
+                currentTime = 0;
+                currentSpeed = walkSpeed;
+            }
         }
     }
 
@@ -63,15 +92,19 @@ public class PlayerController : MonoBehaviour {
             animator.SetBool( "running", running );
             if ( running ) {
                 currentSpeed = runSpeed;
-                Walking = false;
+            } else {
+                currentSpeed = walkSpeed;
             }
-                
         }
     }
 
     #endregion Variables
 
-    void Awake () {
+    void currentTimePenalty( float penalty ) {
+        currentTime = Mathf.Max( 0f, currentTime - penalty );
+    }
+
+    void Awake() {
         animator = gameObject.GetComponent<Animator>();
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
@@ -79,19 +112,35 @@ public class PlayerController : MonoBehaviour {
 
         currentSpeed = walkSpeed;
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    // Update is called once per frame
+    void FixedUpdate() {
         float horz = Input.GetAxis("Horizontal");
         grounded = Physics2D.OverlapBox( groundCheck.position, new Vector2( boxCollider2D.size.x, 0.001f ), 0f, whatIsGround );
 
-        if (horz != 0f ) {
-            if ( !Running )
+        if ( horz != 0f ) {
+            if ( !Running && !FastWalking )
                 Walking = true;
+
+            if ( Input.GetButton( "Run" ) && !Running ) {
+                FastWalking = true;
+
+                if ( !Jumping && !Falling ) {
+                    currentTime += Time.deltaTime;
+                    if ( currentTime > timeUntilMaxSpeed ) {
+                        Running = true;
+                    }
+                }
+
+            }
+
+            if ( Input.GetButtonUp( "Run" ) ) {
+                FastWalking = Running = false;
+            }
 
             transform.position += Vector3.right * horz * currentSpeed * Time.deltaTime;
 
-            if (horz > 0 ) {
+            if ( horz > 0 ) {
                 spriteRenderer.flipX = false;
             } else {
                 spriteRenderer.flipX = true;
@@ -99,19 +148,17 @@ public class PlayerController : MonoBehaviour {
 
         } else {
             Walking = false;
-        }
-
-        if ( Input.GetButtonDown( "Run" ) ) {
-            Running = true;
-        }
-        if ( Input.GetButtonUp( "Run" ) ) {
+            FastWalking = false;
             Running = false;
         }
 
-        Jumping = !grounded;
+        if ( grounded ) {
+            Jumping = false;
 
-        if ( grounded && Input.GetButtonDown( "Jump" ) ) {
-            rigidbody2D.AddForce( Vector2.up * jumpForce );
+            if ( Input.GetButtonDown( "Jump" ) ) {
+                Jumping = true;
+                rigidbody2D.AddForce( Vector2.up * jumpForce );
+            }
         }
 
         Falling = ( rigidbody2D.velocity.y < -0.2f );
